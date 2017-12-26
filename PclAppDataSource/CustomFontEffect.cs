3cea5e5507715fd0789669630a7def2da94b3273
Ben Askren <ben@buildcalc.com>
2017-12-25 21:40:14 -0500

    Initial commit (created PCL solution in VisualStudio Mac)

---
+++ PclAppDataSource/CustomFontEffect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PclAppDataSource
+{
+    class CustomFontEffect : RoutingEffect
+    {
+        protected CustomFontEffect() : base("NetStandardDataSource.CustomFontEffect")
+        {
+        }
+
+        static public void ApplyTo(Xamarin.Forms.Label label)
+        {
+            var effect = new CustomFontEffect();
+            label.Effects.Add(effect);
+        }
+
+    }
+}
---
+++ PclAppDataSource/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PclAppDataSource
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+            var label = new Label
+            {
+                Text = "This is a label"
+            };
+            CustomFontEffect.ApplyTo(label);
+
+            Content = label;
+        }
+    }
+}
